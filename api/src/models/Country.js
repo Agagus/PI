8954f4fb9f0e824fa6c16c3407a3705c94840567
Agustina Zanetti <agus.zanetti4@gmail.com>
2022-11-07 15:24:34 -0300

    pi

--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -4,9 +4,40 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('country', {
+    id: {
+      primaryKey: true,
+      type: DataTypes.STRING,
+    },
+
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+
+    img: {
+      
+    },
+
+    continent: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    capital: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    subregion: {
+      type: DataTypes.STRING,
+    },
+
+    area: {
+      type: DataTypes.STRING,
+    },
+
+    population: {
+      type: DataTypes.INTEGER,
     },
   });
 };
