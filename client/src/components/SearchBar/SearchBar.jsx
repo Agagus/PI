af5a28f574c05777342e9c422bb929d7ebae2fd9
Agustina Zanetti <agus.zanetti4@gmail.com>
2022-11-17 23:42:54 -0300

    pi

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,5 +1,28 @@
+import { useState } from "react"
+import { useDispatch } from "react-redux";
+
 export const SearchBar = () => {
+    const dispatch = useDispatch();
+
+    const [input, setInput] = useState({
+        name: ''
+    });
+
+    const handlerChange = () => {};
+
+    const handlerSubmit = () => {};
+
     return (
-        <span>SearchBar</span>
+        <div>
+            <form onSubmit={handlerSubmit}>
+                <input
+                    type= 'text'
+                    name='name'
+                    value={input.value}
+                    onChange= {handlerChange}></input>
+                <button>Search</button>
+
+            </form>
+        </div>
     )
 } 
