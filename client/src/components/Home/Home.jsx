79becfad890e702918374fd784e05a08e5acf60a
Agustina Zanetti <agus.zanetti4@gmail.com>
2023-01-20 13:26:54 -0300

    adding some styles

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,65 +1,64 @@
-import CountryCards from "../CountryCards/CountryCards"
-import { Pagination } from "../Pagination/Pagination"
-import React, { useEffect, useState } from 'react'
-import { SearchBar } from "../SearchBar/SearchBar"
-import { connect, useDispatch, useSelector } from "react-redux";
+import CountryCards from '../CountryCards/CountryCards';
+import { Pagination } from '../Pagination/Pagination';
+import React, { useEffect, useState } from 'react';
+import { SearchBar } from '../SearchBar/SearchBar';
+import { connect, useDispatch, useSelector } from 'react-redux';
 import { getAllCountries } from '../../redux/actions/index';
-import { Link } from "react-router-dom";
-import style from "./Home.module.css"
-import { Order } from "../OrderButtons/Order";
+import { Link } from 'react-router-dom';
+import style from './Home.module.css';
+import { Order } from '../OrderButtons/Order';
 
 export const Home = () => {
+  const countries = useSelector((state) => state.countries);
+  // const filterCountries = useSelector((state) => state.filterCountries)
+  const dispatch = useDispatch();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [countriesPerPage, setCountriesPerPage] = useState(10);
+  const indexLast = currentPage * countriesPerPage - 1; //
+  const indexFirst = indexLast - countriesPerPage;
+  const currentCountries =
+    currentPage === 1
+      ? countries.slice(0, 9)
+      : countries.slice(indexFirst, indexLast);
 
-    const countries = useSelector((state) => state.countries);
-    // const filterCountries = useSelector((state) => state.filterCountries)
-    const dispatch = useDispatch();
-    const [currentPage, setCurrentPage] = useState(1);
-    const [countriesPerPage, setCountriesPerPage] = useState(10);
-    const indexLast = (currentPage * countriesPerPage) - 1; //
-    const indexFirst = indexLast - countriesPerPage; 
-    const currentCountries = currentPage === 1 ? countries.slice(0, 9) : countries.slice(indexFirst, indexLast)
+  const paginated = (page) => {
+    setCurrentPage(page);
+  };
 
+  useEffect(() => {
+    dispatch(getAllCountries());
+  }, [dispatch]);
 
-    const paginated = (page) => {
-        setCurrentPage(page)
-    }
+  return (
+    <div className={style.container}>
+      <p className={style.nav}>
+        <p className={style.p1}>
+          <SearchBar className={style.sear} />
+          <Link to='/activities'>
+            <button className={style.act}>Add activity</button>
+          </Link>
+        </p>
+        <Order setCurrentPage={setCurrentPage} />
+        <p className={style.p}>
+          <Pagination
+            countries={countries.length}
+            countriesPerPage={countriesPerPage}
+            paginated={paginated}
+          />
+        </p>
+      </p>
 
-    useEffect(()=> {
-        dispatch(getAllCountries())
-    }, [dispatch]);
-
-        return (
-                <div>
-                    <p className={style.nav}>
-                        <p className={style.p1}>
-                            <SearchBar className={style.sear} />
-                            <Link to = '/activities'>
-                                <button className={style.act}>Add activity</button>
-                            </Link> 
-                        </p>
-                            <Order setCurrentPage={setCurrentPage}/>
-                        <p className={style.p}>
-                            <Pagination 
-                                countries={countries.length}
-                                countriesPerPage={countriesPerPage}
-                                paginated={paginated}
-                            />
-                        
-                        </p>
-                    </p>
-                    
-
-                    <div className={style.cards}>
-                    { currentCountries.length && currentCountries.map(country => 
-                            <CountryCards
-                                flags = {country.flags}
-                                name = {country.name}
-                                continent = {country.continent}
-                                id = {country.id}
-                            />
-                    )
-                    }
-                    </div>
-                </div>
-            )
-}
+      <div className={style.cards}>
+        {currentCountries.length &&
+          currentCountries.map((country) => (
+            <CountryCards
+              flags={country.flags}
+              name={country.name}
+              continent={country.continent}
+              id={country.id}
+            />
+          ))}
+      </div>
+    </div>
+  );
+};
--- client/src/components/OrderButtons/Order.jsx
+++ client/src/components/OrderButtons/Order.jsx
@@ -1,86 +1,102 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { getActivities, getByContinent, getByActivity, getAlphabetical, getByPopulation } from "../../redux/actions/index";
-import style from "./Order.module.css"
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  getActivities,
+  getByContinent,
+  getByActivity,
+  getAlphabetical,
+  getByPopulation,
+} from '../../redux/actions/index';
+import style from './Order.module.css';
 
-export const Order = ({setCurrentPage}) => {
-    const dispatch = useDispatch();
-    const activities = useSelector(state => state.activities); 
+export const Order = ({ setCurrentPage }) => {
+  const dispatch = useDispatch();
+  const activities = useSelector((state) => state.activities);
 
-    useEffect(() => {
-        dispatch(getActivities());
-    }, [dispatch])
-    
+  useEffect(() => {
+    dispatch(getActivities());
+  }, [dispatch]);
 
-        
-    const handlerActivities = (e) => {
-        e.preventDefault();
-        dispatch(getByActivity(e.target.value))
-        setCurrentPage(1)
-    }
-    
-    
-    
-    const handlerContinent = (e) => {
-        e.preventDefault();
-        dispatch(getByContinent(e.target.value))
-        setCurrentPage(1)
-    }
+  const handlerActivities = (e) => {
+    e.preventDefault();
+    dispatch(getByActivity(e.target.value));
+    setCurrentPage(1);
+  };
 
+  const handlerContinent = (e) => {
+    e.preventDefault();
+    dispatch(getByContinent(e.target.value));
+    setCurrentPage(1);
+  };
 
-    const handlerOrderA = (e) => {
-        e.preventDefault();
-        dispatch(getAlphabetical(e.target.value))
-        setCurrentPage(1)
-        
+  const handlerOrderA = (e) => {
+    e.preventDefault();
+    dispatch(getAlphabetical(e.target.value));
+    setCurrentPage(1);
+  };
 
-    }
+  const handlerOrderPop = (e) => {
+    e.preventDefault();
+    dispatch(getByPopulation(e.target.value));
+    setCurrentPage(1);
+  };
 
-    const handlerOrderPop = (e) => {
-        e.preventDefault();
-        dispatch(getByPopulation(e.target.value))
-        setCurrentPage(1)
-    }
-
-
-    return (
-        <div className={style.container}>
-            <div>
-                <span>
-                    <select className= {style.btn} onChange = {e => handlerContinent(e)}>
-                        <option select value='All'>All</option>
-                        <option value='Africa'>Africa</option>
-                        <option value='Antarctic'>Antarctic</option>
-                        <option value='Asia'>Asia</option>
-                        <option value='Europe'>Europe</option>
-                        <option value='Oceania'>Oceania</option>
-                        <option value='Americas'>Americas</option>
-                    </select>
-                </span>
-                <span>
-                    <select className={style.btn} onChange={e => handlerActivities(e)}>
-                        <option value='All'>All</option>
-                        { activities && activities.map(a => {
-                            return(<option value={a.name}>{a.name}</option>)
-                        })}
-                    </select>
-                </span>
-                <span>
-                    <select className= {style.btn} onChange={e => handlerOrderA(e)}>
-                        <option name='random' value='random'>-</option>
-                        <option name='asc' value='asc'>A - Z</option>
-                        <option name= 'desc' value='desc'>Z - A</option>
-                    </select>
-                </span>
-                <span>
-                    <select className= {style.btn} onChange={e => handlerOrderPop(e)}>
-                        <option select value='-'>-</option>
-                        <option name='ascP' value='ascP'>Ascending population</option>
-                        <option name='descP' value='descP'>Descending population</option>
-                    </select>
-                </span>
-            </div>
-        </div>
-    )
-
-}
+  return (
+    <div className={style.container}>
+      {/* <div> */}
+      <span>
+        <label>Continents</label>
+        <select className={style.btn} onChange={(e) => handlerContinent(e)}>
+          <option select value='All'>
+            All
+          </option>
+          <option value='Africa'>Africa</option>
+          <option value='Antarctic'>Antarctic</option>
+          <option value='Asia'>Asia</option>
+          <option value='Europe'>Europe</option>
+          <option value='Oceania'>Oceania</option>
+          <option value='Americas'>Americas</option>
+        </select>
+      </span>
+      <span>
+        <label>Activities</label>
+        <select className={style.btn} onChange={(e) => handlerActivities(e)}>
+          <option value='All'>All</option>
+          {activities &&
+            activities.map((a) => {
+              return <option value={a.name}>{a.name}</option>;
+            })}
+        </select>
+      </span>
+      <span>
+        <label>Order alphabetical</label>
+        <select className={style.btn} onChange={(e) => handlerOrderA(e)}>
+          <option name='random' value='random'>
+            -
+          </option>
+          <option name='asc' value='asc'>
+            A - Z
+          </option>
+          <option name='desc' value='desc'>
+            Z - A
+          </option>
+        </select>
+      </span>
+      <span>
+        <label>Order by population</label>
+        <select className={style.btn} onChange={(e) => handlerOrderPop(e)}>
+          <option select value='-'>
+            -
+          </option>
+          <option name='ascP' value='ascP'>
+            Ascending population
+          </option>
+          <option name='descP' value='descP'>
+            Descending population
+          </option>
+        </select>
+      </span>
+      {/* </div> */}
+    </div>
+  );
+};
