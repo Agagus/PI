0a383212b0c4a169f307804e8d7708e093ee7181
Agustina Zanetti <agus.zanetti4@gmail.com>
2023-03-09 21:58:38 -0300

    modify .env

--- api/src/db.js
+++ api/src/db.js
@@ -2,12 +2,12 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
 //psql -h db.sjifbhfwonxaovgjwbbr.supabase.co -p 5432 -d postgres -U postgres
 
 //instance Sequelize to connect to the database
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/countries`,
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
