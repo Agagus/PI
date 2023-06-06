826f3bd58c1214e9b974533bff9e2da29a6b8878
Agagus <agus.zanetti4@gmail.com>
2023-06-06 09:24:50 -0300

    new db deploy

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,28 @@
-import './App.css';
-import React from 'react';
-import { Route } from 'react-router-dom';
-import { LandingPage } from './components/LandingPage/LandingPage';
-import { Home } from './components/Home/Home.jsx';
-import { CountryDetail } from './components/CountryDetail/CountryDetail.jsx';
-import { CreateActivity } from './components/CreateActivity/CreateActivity';
-import style from './App.module.css';
-import axios from 'axios';
-// axios.defaults.baseURL = 'http://localhost:3001';
-axios.defaults.baseURL = 'https://pi-production.up.railway.app';
+import "./App.css";
+import React from "react";
+import { Route } from "react-router-dom";
+import { LandingPage } from "./components/LandingPage/LandingPage";
+import { Home } from "./components/Home/Home.jsx";
+import { CountryDetail } from "./components/CountryDetail/CountryDetail.jsx";
+import { CreateActivity } from "./components/CreateActivity/CreateActivity";
+import style from "./App.module.css";
+import axios from "axios";
+//axios.defaults.baseURL = "http://localhost:3001";
+//axios.defaults.baseURL = 'https://pi-production.up.railway.app'; //link roto
+axios.defaults.baseURL = "https://gratis-oil-production.up.railway.app";
 // import CountryCard from './components/CountryCard/CountryCard.jsx';
 // En este archivo hacer las routes
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <h1 className={style.app}>
         <div className={style.landing}>
-          <Route exact path='/' component={LandingPage} />
+          <Route exact path="/" component={LandingPage} />
         </div>
-        <Route exact path='/countries' render={() => <Home />} />
-        <Route exact path='/countries/:id' render={() => <CountryDetail />} />
-        <Route exact path='/activities' render={() => <CreateActivity />} />
+        <Route exact path="/countries" render={() => <Home />} />
+        <Route exact path="/countries/:id" render={() => <CountryDetail />} />
+        <Route exact path="/activities" render={() => <CreateActivity />} />
       </h1>
     </div>
   );
