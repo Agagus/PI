d5a30153cd0a344436ee26de695db8db7708e66c
Agagus <agus.zanetti4@gmail.com>
2023-06-06 09:42:59 -0300

    new deploy

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,8 @@ import style from "./App.module.css";
 import axios from "axios";
 //axios.defaults.baseURL = "http://localhost:3001";
 //axios.defaults.baseURL = 'https://pi-production.up.railway.app'; //link roto
-axios.defaults.baseURL = "https://gratis-oil-production.up.railway.app";
+axios.defaults.baseURL =
+  "https://postgres://countries_ubzs_user:TQ1ZVsQq12bRvuLRNoet8gSkUJ4Kloo0@dpg-chvii3pmbg5b5p8rk7m0-a/countries_ubzs";
 // import CountryCard from './components/CountryCard/CountryCard.jsx';
 // En este archivo hacer las routes
 
